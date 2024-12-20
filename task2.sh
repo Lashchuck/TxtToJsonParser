5f41219c6589f45adfbea9a01c517c2f3988e611
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-20 17:42:47 +0100

    task2

--- task2.sh
+++ task2.sh
@@ -29,8 +29,8 @@ convert_to_json() {
             for (i = 3; i <= NF - 2; i++) name = name " " $i
             name = name " " $(NF - 1)  # Add the second last field for the full name
             gsub(/^\s*[0-9]+\s/, "", name)  # Remove leading test numbers
+            gsub(/^ /, "", name)            # Trim leading space
 
-            gsub(/, $/, "", name)           # Remove trailing comma
             printf "{name:\"%s\",status:%s,duration:\"%s\"},\n", name, status, $NF
         }
         END { print "]" }
