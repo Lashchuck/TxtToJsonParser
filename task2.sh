ab6c2281f91ec6561c62408733ad48a00245bafb
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-20 18:33:07 +0100

    task2

--- task2.sh
+++ task2.sh
@@ -28,7 +28,7 @@ convert_to_json() {
             name = ""
             for (i = 3; i <= NF - 2; i++) name = name " " $i
             name = name " " $(NF - 1)  # Add the second last field for the full name
-            gsub(/^[0-9]+\s+/, "", name)  # Remove leading numbers and spaces
+            gsub(/^\s*[0-9]+\s/, "", name)  # Remove leading test numbers
             gsub(/^ /, "", name)            # Trim leading space
             gsub(/, $/, "", name)           # Remove trailing comma
             gsub(/,$/, "", name)            # Remove comma before ending quote
