dc7dbec1e144c42a53b94b20f78b4efbe4aef8f0
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-20 18:48:31 +0100

    task2

--- task2.sh
+++ task2.sh
@@ -27,7 +27,6 @@ convert_to_json() {
 
     local tests_json
     tests_json=$(grep -P '^(ok|not ok)' "$input_file" | \
-        sed -E 's/^[^0-9]*[0-9]+\s+//' | \
         awk '
         BEGIN { print "[" }
         {
@@ -35,7 +34,7 @@ convert_to_json() {
             name = ""
             for (i = 3; i <= NF - 2; i++) name = name " " $i
             name = name " " $(NF - 1)  # Add the second last field for the full name
-            gsub(/,\s*$/, "", name)
+            gsub(/^\s*[0-9]+\s/, "", name)
             gsub(/,\s*$/, "", name)       # Remove trailing commas after name
             gsub(/^ /, "", name)          # Trim leading spaces
             printf "{\"name\":\"%s\",\"status\":%s,\"duration\":\"%s\"},\n", name, status, $NF
